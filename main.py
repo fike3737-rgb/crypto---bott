4b2a2f0f2c642c9049c482454c02401d0a7e8c15
fike3737-rgb <fike3737@gmail.com>
2026-09-11 01:54:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def analyze_market(message):
 
 if __name__ == "__main__":
     keep_alive()
-    print("ቦቱ በሰላም ስራ ጀምሯል...")
-    bot.infinity_polling(skip_pending=True, timeout=90, long_polling_timeout=90)
+    print("ቦቱ በሰላም ስራ ጀምሯል.
+bot.infinity_polling(skip_pending=True)
 
