802d8a29153e14d2dcfa9b2061db739f9abc3b9d
fike3737-rgb <fike3737@gmail.com>
2026-09-11 23:31:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ client = Groq(
 )
 
 response = client.chat.completions.create(
-    model="llama3-8b-8192",
+    model="llama-3.3-70b-versatile",
     messages=[
         {
             "role": "system",
