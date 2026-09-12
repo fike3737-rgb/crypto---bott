4ae848567a314e6b819b51590d7404357eeea136
fike3737-rgb <fike3737@gmail.com>
2026-09-12 22:37:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def analyze_market(message):
                     "content": f"Provide a detailed financial market analysis and technical levels for: {user_query}. Respond in English.",
                 }
             ],
-            model="llama-3.1-8b-instant",
+            model = "mixtral-8x7b-32768",
         )
         response_text = chat_completion.choices[0].message.content
         bot.reply_to(message, response_text)
