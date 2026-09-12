a18c739ade6423ff59599dfcf1717930292b7797
fike3737-rgb <fike3737@gmail.com>
2026-09-13 00:45:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
+from telegram.ext import ApplicationBuilder
 
+# 1. ፖርቱን ከሬንደር መቀበል
 PORT = int(os.environ.get('PORT', '8443'))
 
-# ቦቱ ዌብሆክን ተጠቅሞ እንዲሰራ ማድረግ
+# 2. የቦት ቶከን
+BOT_TOKEN = "8703693504:AAE5dfXRocJZupznR2k5XD3Eec02n8KDCFI"
+
+# 3. application የሚለውን ተለዋዋጭ መፍጠር (እዚህ ጋር ነው የጎደለው)
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+# 4. ዌብሆክን ማስጀመር
 application.run_webhook(
     listen="0.0.0.0",
     port=PORT,
-    url_path="8703693504:AAE5dfXRocJZupznR2k5XD3Eec02n8KDCFI",  # የቦትህ ቶከን
-    webhook_url="https://crypto--bott.onrender.com/8703693504:AAE5dfXRocJZupznR2k5XD3Eec02n8KDCFI" # የሬንደር ሊንክህ ከቶከኑ ጋር
+    url_path=BOT_TOKEN,
+    webhook_url=f"https://crypto--bott.onrender.com/{BOT_TOKEN}"
 )
 
